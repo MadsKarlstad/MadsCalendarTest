998e774ebf781e1f809a246907ebb42f509a2e22
madsmkarlstad <mads.karlstad@me.com>
2016-09-01 16:40:08 +0200

    WO-2 Seperated the css

--- script.js
+++ script.js
@@ -34,9 +34,8 @@ calendarApp.controller('calendarController', function ($scope) {
     //$scope.rooms = [{"name" : "Northern Hall", "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00"], "bookings" : [{"description" : "Football practice", "from" : "08:00", "to" : "09:00"},{"description" : "Handball practice", "from" : "10:00", "to" : "11:00"}]}, {"name" : "Southern Hall", "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00"], "bookings" : [{"description" : "Football practice", "from" : "08:00", "to" : "09:00"}]}];
 
 
-
     console.log($scope.facilities);
 
-    $scope.message = 'Hello world';
+    $scope.message = 'Homework for Mads';
 
 });
