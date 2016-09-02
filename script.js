7f923c61cdb0ad5669bf7ac6dea55595af8f34bf
madsmkarlstad <mads.karlstad@me.com>
2016-09-02 10:42:49 +0200

    WO-2 changed to div, added moment.js

--- script.js
+++ script.js
@@ -21,16 +21,14 @@ calendarApp.config(function ($routeProvider) {
 // create the controller
 calendarApp.controller('calendarController', function ($scope) {
 
-    var date = new Date();
+    var date = moment().format('YYYY-MM-DD');
     $scope.date = date;
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-    console.log(date);
+    var day = moment().format('dddd');
+    $scope.day = day;
 
     $scope.facilities = [
         {"id" : "1",
-        "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00"],
+        "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00"],
         "rooms" :   [
             {"name" : "Northern Hall", "bookings" : [
                 {"description" : "Football practice", "from" : "08:00", "to" : "09:00"},
