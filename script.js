a5ec594f4ccb7433573b1979b8b2e9ced1e44219
madsmkarlstad <mads.karlstad@me.com>
2016-09-05 10:35:22 +0200

    WO-3 making things more responsive

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ calendarApp.controller('calendarController', function ($scope) {
 
     $scope.facilities = [
         {"id" : "1",
-        "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00"],
+        "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00"],
         "rooms" :   [
             {"name" : "Northern Hall", "bookings" : [
                 {"description" : "Football practice", "from" : "08:00", "to" : "09:00"},
@@ -40,6 +40,7 @@ calendarApp.controller('calendarController', function ($scope) {
             ]}
         ]}
     ];
+
 
     //$scope.rooms = [{"name" : "Northern Hall", "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00"], "bookings" : [{"description" : "Football practice", "from" : "08:00", "to" : "09:00"},{"description" : "Handball practice", "from" : "10:00", "to" : "11:00"}]}, {"name" : "Southern Hall", "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00"], "bookings" : [{"description" : "Football practice", "from" : "08:00", "to" : "09:00"}]}];
 
