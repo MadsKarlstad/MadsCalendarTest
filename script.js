3b3f2b51fc42c40093260ebdbfe0e26be6fbfc6e
madsmkarlstad <mads.karlstad@me.com>
2016-09-05 16:12:31 +0200

    WO-3 added service for the controller and changing between days

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // create the module
 var calendarApp = angular.module('calendarApp',['ngRoute']);
 
+//create routing
 calendarApp.config(function ($routeProvider) {
     $routeProvider
 
@@ -18,36 +19,80 @@ calendarApp.config(function ($routeProvider) {
         });
 });
 
-// create the controller
-calendarApp.controller('calendarController', function ($scope) {
-
+//create the service
+calendarApp.service('sharedProperties', function() {
     var date = moment().format('DD-MM-YYYY');
-    $scope.date = date;
     var day = moment().format('dddd');
-    $scope.day = day;
-
-    $scope.facilities = [
+    var facilities = [
         {"id" : "1",
-        "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00"],
-        "rooms" :   [
-            {"name" : "Northern Hall", "bookings" : [
-                {"description" : "Football practice", "date" : date, "from" : "08:00", "to" : "09:00"},
-                {"description" : "Handball practice", "date" : date, "from" : "10:00", "to" : "11:00"},
-                {"description" : "Conference", "date" : moment(date, 'DD-MM-YYYY').add('d',1).format('DD-MM-YYYY'), "from" : "10:00", "to" : "11:00"}
-            ]},
-            {"name" : "Southern Hall", "bookings" : [
-                {"description" : "Bandy practice", "date" : date, "from" : "08:00", "to" : "09:00"}
-            ]}
-        ]}
+            "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00"],
+            "rooms" :   [
+                {"name" : "Northern Hall", "bookings" : [
+                    {"description" : "Football practice", "date" : date, "from" : "08:00", "to" : "09:00"},
+                    {"description" : "Handball practice", "date" : date, "from" : "10:00", "to" : "11:00"},
+                    {"description" : "Conference", "date" : date, "from" : "10:00", "to" : "11:00"}
+                ]},
+                {"name" : "Southern Hall", "bookings" : [
+                    {"description" : "Bandy practice", "date" : date, "from" : "08:00", "to" : "09:00"}
+                ]}
+            ]
+        }
     ];
 
-    $scope.changeDay = function (dato) {
-        date = dato;
+    return {
+        //returns the date
+        getDate: function () {
+            return date;
+        },
+        //sets the date
+        setDate: function (value) {
+            date = value;
+        },
+        //returns the day
+        getDay: function () {
+            return day;
+        },
+        //sets the day
+        setDay: function (value) {
+            day = value;
+        },
+        //returns the facility object
+        getFacilities: function () {
+            return facilities;
+        },
+        //returns the bookings for a given date
+        getBookingsBasedOnDate: function () {
+            facilities = [
+                {"id" : "1",
+                    "availability" : ["08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00"],
+                    "rooms" :   [
+                        {"name" : "Northern Hall", "bookings" : []},
+                        {"name" : "Southern Hall", "bookings" : []}
+                    ]
+                }
+            ];
+            return facilities;
+        }
+    }
+});
+// create the controller
+calendarApp.controller('calendarController', function ($scope, sharedProperties) {
 
+    $scope.init = function () {
+        $scope.date = sharedProperties.getDate();
+        $scope.day = sharedProperties.getDay();
+        $scope.facilities = sharedProperties.getFacilities();
     };
 
-    $scope.test = function () {
-      console.log("Halla verden")
+    $scope.nextDay = function () {
+        $scope.day = sharedProperties.setDay(moment(sharedProperties.getDate(), 'DD-MM-YYYY').add(1,'d').format('dddd'));
+        $scope.date = sharedProperties.setDate(moment(sharedProperties.getDate(), 'DD-MM-YYYY').add(1,'d').format('DD-MM-YYYY'));
+        $scope.facilities = sharedProperties.getBookingsBasedOnDate();
+    };
+
+    $scope.prevDay = function () {
+        $scope.day = sharedProperties.setDay(moment(sharedProperties.getDate(), 'DD-MM-YYYY').subtract(1,'d').format('dddd'));
+        $scope.date = sharedProperties.setDate(moment(sharedProperties.getDate(), 'DD-MM-YYYY').subtract(1,'d').format('DD-MM-YYYY'));
     };
 
     $scope.message = 'Homework for Mads';
